9800cffbd64cfdbfc568fab236091a1d18f457e0
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-07 20:38:49 +0200

    add static templates

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+setup(
+    name="dtreg",
+    version="0.1",
+    description="Clean package",
+    packages=find_packages() + ['templates'],
+    include_package_data=True
+)
 if __name__ == '__main__':
     setup()

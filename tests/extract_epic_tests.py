f0cd3f6eac3d7d90a6c6d749f18ed589890e7648
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-07 10:31:46 +0200

    add test extract_epic

---
+++ tests/extract_epic_tests.py
@@ -0,0 +1,11 @@
+import unittest
+class TestRequest(unittest.TestCase):
+
+    def test_obtain(self):
+        result = extract_epic("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
+        expected = {'PIDINST-SchemaObject': [[{'name': 'PIDINST-SchemaObject',
+            'identifier': '21.T11969/1ea0e148d9bbe08335cd',
+            'schema_type': 'Object'}]]}
+        self.assertEqual(result, expected)        
+if __name__ == '__main__':
+    unittest.main()

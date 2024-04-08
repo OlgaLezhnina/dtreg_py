294e92a3d5577ce157c1d0f4302f594d2499da79
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-08 20:20:52 +0200

    change import

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
-    name="dtreg",
-    version="0.1",
-    description="Clean package",
-    packages=find_packages() + ['templates'],
-    include_package_data=True
+    name = "dtreg",
+    version = "0.1",
+    description = "Clean package",
+    packages = find_packages() + ['templates'],
+    include_package_data = True
 )
 if __name__ == '__main__':
     setup()

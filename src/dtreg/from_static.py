67c5d86b03b2eed1b0b02961fd278b7be3026389
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-10 11:23:13 +0200

    read static data

--- src/dtreg/from_static.py
+++ src/dtreg/from_static.py
@@ -1,8 +1,2 @@
-import importlib_resources
-resources = importlib_resources.files("dtreg")
-template_1 = resources.joinpath("src/dtreg/data", "empty_schema.json").read_text()
-
 from importlib_resources import files
-import setuptools 
-
-data_text = files("dtreg.data").joinpath("empty_schema.json").read_text()
+template_1 = files("dtreg.data").joinpath("empty_schema.json").read_text()

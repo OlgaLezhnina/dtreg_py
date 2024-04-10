80ca78f0dfb071489aa2e544848db668aeb35963
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-10 23:21:30 +0200

    add dtr protocol

---
+++ src/dtreg/dtr_interface.py
@@ -0,0 +1,22 @@
+from typing import Protocol
+from .extract_epic import extract_epic
+from .from_static import from_static
+
+class DataTypeReg(Protocol):
+    def get_template_info(self, template_doi):
+        pass
+
+class Epic:
+    def get_template_info(self, template_doi):
+        static = from_static(template_doi)
+        if static is None:
+           template_info = extract_epic(template_doi)
+        else:
+           template_info = static
+        return template_info       
+      
+def select_dtr(template_doi):
+    if template_doi.split("/", 4)[3] == '21.T11969':
+        datypreg = Epic()
+        return datypreg
+  

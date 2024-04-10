5fdf50ad1310e78a6c2a0257cf952dac63db8f2d
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-10 23:22:03 +0200

    add from_static

--- src/dtreg/from_static.py
+++ src/dtreg/from_static.py
@@ -1,2 +1,14 @@
+import json
 from importlib_resources import files
-template_1 = files("dtreg.data").joinpath("empty_schema.json").read_text()
+
+def from_static(template_id):
+    id = template_id.split("/", 3)[3]
+    final_template = None
+    all_static_data = []
+    for f in files("dtreg.data").iterdir():
+        text = f.read_text()
+        all_static_data.append(text)
+    for static in all_static_data:
+        if id in static:
+            final_template = json.loads(static)
+    return final_template

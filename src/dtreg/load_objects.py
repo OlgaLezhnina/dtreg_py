f104c1a2753e92bdb439bb21dca1e12f2d35d811
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-14 20:04:17 +0200

    add load_objects functions

---
+++ src/dtreg/load_objects.py
@@ -0,0 +1,30 @@
+from .dtr_interface import supply_dtr_with_info
+
+def load_objects(template_doi):
+    datypreg, templ_info = supply_dtr_with_info(template_doi)
+    result = write_objects(templ_info)
+    return result
+
+def write_objects(templ_info):
+    objects = {}
+    for key in templ_info.keys():
+        name = templ_info[key][0][0]["name"]
+        ObjName = "Obj_" + name
+        # get prop names
+        prop_list = [] 
+        if len(templ_info[key][1]) != 0:
+            for props in templ_info[key][1]:
+                prop_list.append(props["prop_name"]) 
+        def __init__(self, *args, **kwargs):
+            for prop_name in prop_list:
+                setattr(self, prop_name, kwargs.get(prop_name))                
+        ObjName = type(name, 
+                 (),                    
+                 {"name": name, 
+                  "identifier": templ_info[key][0][0]["identifier"] ,
+                  "schema_type": templ_info[key][0][0]["schema_type"],
+                  "prop_list":prop_list,
+                  "__init__": __init__}) 
+        objects.update({name: ObjName})
+    return objects
+    

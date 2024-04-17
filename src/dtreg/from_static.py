79397767efc8fe514aa8c32545a8de108159bf1b
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-17 16:41:33 +0200

    adjust from_static to new data files

--- src/dtreg/from_static.py
+++ src/dtreg/from_static.py
@@ -1,14 +1,12 @@
 import json
+#from pathlib import Path
 from importlib_resources import files
 
 def from_static(template_id):
-    id = template_id.split("/", 3)[3]
+    id = template_id.split("/", 4)[4]
     final_template = None
-    all_static_data = []
     for f in files("dtreg.data").iterdir():
-        text = f.read_text()
-        all_static_data.append(text)
-    for static in all_static_data:
-        if id in static:
-            final_template = json.loads(static)
+        if id in f.stem:            
+            file_text = f.read_text()
+            final_template = json.loads(file_text)
     return final_template

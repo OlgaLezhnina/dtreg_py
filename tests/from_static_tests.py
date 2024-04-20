ca18f8c2bd7ba2871cc2c8ca809dceb5f9a03b92
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-20 17:55:41 +0200

    add no_static

--- tests/from_static_tests.py
+++ tests/from_static_tests.py
@@ -1,9 +1,13 @@
 import unittest
 from dtreg.from_static import from_static
 class TestFromStatic(unittest.TestCase):
+    
+    def test_no_static(self):
+        no_static = from_static("https://doi.org/21.T11969/111")        
+        self.assertEqual(no_static, None)
 
     def test_static(self):
-        templ = from_static("https://doi.org/22.B34567/1ea0e148d9bbe08335cd")
+        templ = from_static("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
         expected = {'PIDINST-SchemaObject': [[{'name': 'PIDINST-SchemaObject',
             'identifier': '21.T11969/1ea0e148d9bbe08335cd',
             'schema_type': 'Object'}],

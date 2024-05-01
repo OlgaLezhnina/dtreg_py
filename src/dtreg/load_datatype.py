8e3e21765ee477606a4618aefb6f78748f51f2b5
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-05-01 13:05:12 +0200

    fix the late binding closure

--- src/dtreg/load_datatype.py
+++ src/dtreg/load_datatype.py
@@ -11,22 +11,20 @@ def write_objects(templ_info):
     objects = {}
     for key in templ_info.keys():
         name = templ_info[key][0][0]["name"]
-        ObjName = "Obj_" + name
-        # get prop names
         prop_list = [] 
         if len(templ_info[key][1]) != 0:
             for props in templ_info[key][1]:
                 prop_list.append(props["prop_name"]) 
         def __init__(self, *args, **kwargs):
-            for prop_name in prop_list:
+            for prop_name in self.prop_list:
                 setattr(self, prop_name, kwargs.get(prop_name))                
-        ObjName = type(name, 
+        class_object = type(name, 
                  (),                    
                  {"name": name, 
                   "identifier": templ_info[key][0][0]["identifier"] ,
                   "schema_type": templ_info[key][0][0]["schema_type"],
                   "prop_list":prop_list,
                   "__init__": __init__}) 
-        objects.update({name: ObjName})
+        objects.update({name: class_object})
     return objects
     

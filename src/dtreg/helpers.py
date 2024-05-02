e3b7144cf8ca09da435cf335a5e9b41f296fc61b
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-05-02 18:34:22 +0200

    add generate_uid

--- src/dtreg/helpers.py
+++ src/dtreg/helpers.py
@@ -1,2 +1,10 @@
 def format_string(text):
   return text.lower().replace(" ", "_").replace("-", "_")
+
+def generate_uid():
+    i = 0
+    def function():
+        nonlocal i
+        i += 1
+        return i
+    return function 

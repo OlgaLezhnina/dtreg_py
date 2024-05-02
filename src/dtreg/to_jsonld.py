0399565e2346c67d21710ff0d5c5067eb1eae1d8
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-05-02 20:40:07 +0200

    fix the pass and add context

--- src/dtreg/to_jsonld.py
+++ src/dtreg/to_jsonld.py
@@ -1,30 +1,36 @@
+from .helpers import generate_uid
 import pandas as pd
 
 def differ_type(input):
     if isinstance(input, pd.DataFrame):
         output = df_structure(input)    
-    elif input is None:
-        output = None
     else:
-        output = list(input)
+        output = input
     return output
 
 def df_structure(df, label):
-    return None
+    pass
     
 def to_jsonld(instance):
     result_all = {}
+    uid = generate_uid()
+    context = {}
+    context[instance.identifier] = ("https://doi.org/"+ instance.identifier)
     def write_info(instance):
         result = {}
-        result["label"] = instance.label
+        result["@id"] = "_:n" + str(uid())
+        result["@label"] = instance.label
+        result["@type"] = "https://doi.org/" + instance.identifier
         field_list = instance.prop_list
         for field in field_list:
             instance_field = getattr(instance, field)
             if hasattr(instance_field, "prop_list"):
-                print(instance_field)
-                result[field] = list(write_info(instance_field))
+                result[field] = write_info(instance_field)
+            elif instance_field is None:
+                pass
             else:
-                result[field] = "dunno"
+                result[field] = differ_type(instance_field)
         return result
-    result_all[instance.identifier] = write_info(instance)
+    result_all[instance.name] = write_info(instance)
+    result_all["@context"] = context
     return result_all

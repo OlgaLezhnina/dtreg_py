322ad8c1b3205b8fb51e753979756dc6bb744d5b
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-05-24 17:41:29 +0200

    align extractor_epic to new metaschema

--- src/dtreg/extract_epic.py
+++ src/dtreg/extract_epic.py
@@ -1,32 +1,34 @@
-from .request_epic import request_epic
+from .request_dtr import request_dtr
 
-def extract_epic(template_doi):
+def extract_epic(dt_id):
     extract_all = {}
-    def extractor_function(template_doi):
-        info = request_epic(template_doi)
+    def extractor_function(dt_id):
+        info = request_dtr(dt_id + "?locatt=view:json")
         schema_dict = {
-            "name": info["name"],
-            "identifier": info["Identifier"],
-            "schema_type": info["Schema"]["Type"]}
+            "dt_name": info["name"],
+            "dt_id": info["Identifier"],
+            "dt_class": info["Schema"]["Type"]}
         extracted = [[schema_dict]]
         all_props = []
         for prop in info["Schema"].get("Properties", []):
             if "Type" in prop:
                 specific_prop_dict = {
-                    "prop_name": prop["Name"],
-                    "type_id": prop["Type"],
-                    "cardinality": prop["Properties"]["Cardinality"],
+                    "dtp_name": prop["Name"],
+                    "dtp_id": info["name"] + "#" + prop["Name"],
+                    "dtp_cardinality": prop["Properties"]["Cardinality"],
+                    "dtp_value_class": prop["Type"],
                     "nested": True}
                 extractor_function("https://doi.org/" + prop["Type"])
             else:        
                 specific_prop_dict = {
-                    "prop_name": prop["Property"],
-                    "type_id": "value",
-                    "cardinality": "no_info",
+                    "dtp_name": prop["Property"],
+                    "dtp_id": info["name"] + "#" + prop["Property"],
+                    "dtp_cardinality": "no_info",
+                    "dtp_value_class": prop["Value"],
                     "nested": False}   
             all_props.append(specific_prop_dict)
         extracted.append(all_props)
-        extract_all[schema_dict["name"]] = list(extracted) 
+        extract_all[schema_dict["dt_name"]] = list(extracted) 
         return(extract_all)
-    extractor_function(template_doi)
+    extractor_function(dt_id)
     return(extract_all)

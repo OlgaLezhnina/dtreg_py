920e5e691cb502bfe5b04a441a794644e8e02cf3
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-06-05 21:56:14 +0200

    adjust test to formatted strings

--- tests/load_datatype_tests.py
+++ tests/load_datatype_tests.py
@@ -4,7 +4,7 @@ class TestLoadObjects(unittest.TestCase):
 
     def test_load_proxies(self):
         obj = load_datatype("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
-        self.assertEqual(next(iter(obj.__dict__)), 'PIDINST-SchemaObject')
+        self.assertEqual(next(iter(obj.__dict__)), 'pidinst_schemaobject')
         
 if __name__ == '__main__':
     unittest.main()

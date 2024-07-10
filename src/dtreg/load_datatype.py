b5ce1cfe2b9ee52c75eae9197cc9736786530e02
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-10 20:32:20 +0200

    rename

--- src/dtreg/load_datatype.py
+++ src/dtreg/load_datatype.py
@@ -2,21 +2,21 @@ from .dtr_interface import select_dtr
 from .helpers import get_prefix
 from types import SimpleNamespace
 
-def load_datatype(template_doi):
-    result_dict = write_objects(template_doi)
-    result = SimpleNamespace(**result_dict)
-    return result
+def load_datatype(datatype_id):
+    schemata_dict = write_classes(datatype_id)
+    schemata = SimpleNamespace(**schemata_dict)
+    return schemata
 
-def write_objects(template_doi):
-    datypreg = select_dtr(template_doi)
-    templ_info = datypreg().get_template_info(template_doi)
-    prefix = get_prefix(template_doi)
+def write_classes(datatype_id):
+    datypreg = select_dtr(datatype_id)
+    schema_info = datypreg().get_schema_info(datatype_id)
+    prefix = get_prefix(datatype_id)
     objects = {}
-    for key in templ_info.keys():
-        dt_name = templ_info[key][0][0]["dt_name"]
+    for key in schema_info.keys():
+        dt_name = schema_info[key][0][0]["dt_name"]
         prop_list = [] 
-        if len(templ_info[key][1]) != 0:
-            for props in templ_info[key][1]:
+        if len(schema_info[key][1]) != 0:
+            for props in schema_info[key][1]:
                 prop_list.append(props["dtp_name"]) 
         def __init__(self, *args, **kwargs):
             for dtp_name in self.prop_list:
@@ -25,10 +25,10 @@ def write_objects(template_doi):
                  (datypreg,),                    
                  {"prefix": prefix,
                   "dt_name": dt_name, 
-                  "dt_id": templ_info[key][0][0]["dt_id"] ,
-                  "dt_class": templ_info[key][0][0]["dt_class"],
+                  "dt_id": schema_info[key][0][0]["dt_id"] ,
+                  "dt_class": schema_info[key][0][0]["dt_class"],
                   "prop_list": prop_list,
-                  "prop_info": templ_info[key][1],
+                  "prop_info": schema_info[key][1],
                   "__init__": __init__}) 
         objects.update({dt_name: class_object})
     return objects

8ea98b5587e6bc2dd7e817baccadbfe3ca7da310
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-10 19:00:34 +0200

    rename

--- src/dtreg/from_static.py
+++ src/dtreg/from_static.py
@@ -1,12 +1,11 @@
 import json
-#from pathlib import Path
 from importlib_resources import files
 
-def from_static(template_id):
-    id = template_id.split("/", 4)[4]
-    final_template = None
+def from_static(datatype_id):
+    id = datatype_id.split("/", 4)[4]
+    schema_info = None
     for f in files("dtreg.data").iterdir():
         if id in f.stem:            
             file_text = f.read_text()
-            final_template = json.loads(file_text)
-    return final_template
+            schema_info = json.loads(file_text)
+    return schema_info

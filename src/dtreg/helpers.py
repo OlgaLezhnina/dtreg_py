34644f10b82d5efb5f3ec5e973fa3f14501585ee
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-21 17:38:58 +0200

    document

--- src/dtreg/helpers.py
+++ src/dtreg/helpers.py
@@ -1,8 +1,20 @@
 def format_string(string):
+    """
+    Format a string to lowercase, spaces and dashes as underscores
+
+    :param string: a string
+    :return: the string formatted in a required way
+    """
     return string.lower().replace(" ", "_").replace("-", "_")
 
 
 def get_prefix(url_string):
+    """
+    Get a prefix of a URL string
+
+    :param url_string: a URL string
+    :return: the prefix string indicating a datatype
+    """
     part = url_string.split("/", 4)
     if "orkg.org" in url_string.split("/", 4)[2]:
         prefix = part[0] + "//" + part[2] + "/"
@@ -12,6 +24,12 @@ def get_prefix(url_string):
 
 
 def specify_cardinality(cardinality_string):
+    """
+    Write cardinality of an ePIC property as dictionary
+
+    :param cardinality_string: an ePIC string for cardinality
+    :return: a dictionary with "min" and "max" values as integers or None
+    """
     card_parts = cardinality_string.split(" - ")
     if len(card_parts) == 1:
         min_max = {
@@ -25,6 +43,10 @@ def specify_cardinality(cardinality_string):
 
 
 def generate_uid():
+    """
+    Generate a counting function for assigning identifiers
+    :return: the counting function
+    """
     i = 0
 
     def function():

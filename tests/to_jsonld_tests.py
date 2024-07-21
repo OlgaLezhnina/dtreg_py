598696cba20ccdeb332f198c0de67002012d55e1
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-21 10:07:32 +0200

    unit tests to_jsonld

--- tests/to_jsonld_tests.py
+++ tests/to_jsonld_tests.py
@@ -42,7 +42,37 @@ class TestJsonLd(unittest.TestCase):
                 result = differ_input(df)
                 self.assertEqual(result["rows"][0]["cells"][0]["value"], None)
 
-    def test_jsonld_epic(self):
+    def test_jsonld_epic_nested(self):
+        dt = load_datatype("https://doi.org/21.T11969/74bc7748b8cd520908bc")
+        table = dt.table(label="Table")
+        instance = dt.inferential_test_output(has_format=table)
+        result = to_jsonld(instance)
+        expected = ('{\n'
+                    '  "inferential_test_output": {\n'
+                    '    "@id": "_:n1",\n'
+                    '    "@type": "doi:74bc7748b8cd520908bc",\n'
+                    '    "doi:74bc7748b8cd520908bc#has_format": {\n'
+                    '      "@id": "_:n2",\n'
+                    '      "@type": "doi:0424f6e7026fa4bc2c4a",\n'
+                    '      "doi:0424f6e7026fa4bc2c4a#label": "Table"\n'
+                    '    }\n'
+                    '  },\n'
+                    '  "@context": {\n'
+                    '    "doi": "https://doi.org/21.T11969/",\n'
+                    '    "columns": "https://doi.org/21.T11969/0424f6e7026fa4bc2c4a#columns",\n'
+                    '    "col_number": "https://doi.org/21.T11969/65ba00e95e60fb8971e6#number",\n'
+                    '    "col_titles": "https://doi.org/21.T11969/65ba00e95e60fb8971e6#titles",\n'
+                    '    "rows": "https://doi.org/21.T11969/0424f6e7026fa4bc2c4a#rows",\n'
+                    '    "row_number": "https://doi.org/21.T11969/9bf7a8e8909bfd491b38#number",\n'
+                    '    "row_titles": "https://doi.org/21.T11969/9bf7a8e8909bfd491b38#titles",\n'
+                    '    "cells": "https://doi.org/21.T11969/9bf7a8e8909bfd491b38#cells",\n'
+                    '    "column": "https://doi.org/21.T11969/4607bc7c42ac8db29bfc#column",\n'
+                    '    "value": "https://doi.org/21.T11969/4607bc7c42ac8db29bfc#value"\n'
+                    '  }\n'
+                    '}')
+        self.assertEqual(result, expected)
+
+    def test_jsonld_epic_list(self):
         dt = load_datatype("https://doi.org/21.T11969/74bc7748b8cd520908bc")
         url_1 = dt.url()
         url_2 = dt.url()
@@ -73,6 +103,61 @@ class TestJsonLd(unittest.TestCase):
                     '    "cells": "https://doi.org/21.T11969/9bf7a8e8909bfd491b38#cells",\n'
                     '    "column": "https://doi.org/21.T11969/4607bc7c42ac8db29bfc#column",\n'
                     '    "value": "https://doi.org/21.T11969/4607bc7c42ac8db29bfc#value"\n'
+                    '  }\n'
+                    '}')
+        self.assertEqual(result, expected)
+
+    def test_jsonld_orkg(self):
+        dt = load_datatype("https://incubating.orkg.org/template/R855534")
+        df = pd.DataFrame({'A': [1]})
+        instance = dt.inferential_test_output(has_format=df)
+        result = to_jsonld(instance)
+        expected = ('{\n'
+                    '  "inferential_test_output": {\n'
+                    '    "@id": "_:n1",\n'
+                    '    "@type": "orkgr:R855534",\n'
+                    '    "orkgp:P114000": {\n'
+                    '      "@type": "orkgc:Table",\n'
+                    '      "label": "Table",\n'
+                    '      "columns": [\n'
+                    '        {\n'
+                    '          "@type": "orkgc:Column",\n'
+                    '          "col_titles": "A",\n'
+                    '          "col_number": 1,\n'
+                    '          "@id": "_:n2"\n'
+                    '        }\n'
+                    '      ],\n'
+                    '      "rows": [\n'
+                    '        {\n'
+                    '          "@type": "orkgc:Row",\n'
+                    '          "row_number": 1,\n'
+                    '          "row_titles": "0",\n'
+                    '          "cells": [\n'
+                    '            {\n'
+                    '              "@type": "orkgc:Cell",\n'
+                    '              "value": "1",\n'
+                    '              "column": "_:n2"\n'
+                    '            }\n'
+                    '          ]\n'
+                    '        }\n'
+                    '      ],\n'
+                    '      "@id": "_:n3"\n'
+                    '    }\n'
+                    '  },\n'
+                    '  "@context": {\n'
+                    '    "orkgc": "https://incubating.orkg.org/class/",\n'
+                    '    "orkgr": "https://incubating.orkg.org/resource/",\n'
+                    '    "orkgp": "https://incubating.orkg.org/property/",\n'
+                    '    "columns": "https://incubating.orkg.org/property/CSVW_Columns",\n'
+                    '    "col_number": "https://incubating.orkg.org/property/CSVW_Number",\n'
+                    '    "col_titles": "https://incubating.orkg.org/property/CSVW_Titles",\n'
+                    '    "rows": "https://incubating.orkg.org/property/CSVW_Rows",\n'
+                    '    "row_number": "https://incubating.orkg.org/property/CSVW_Number",\n'
+                    '    "row_titles": "https://incubating.orkg.org/property/CSVW_Titles",\n'
+                    '    "cells": "https://incubating.orkg.org/property/CSVW_Cells",\n'
+                    '    "column": "https://incubating.orkg.org/property/CSVW_Column",\n'
+                    '    "value": "https://incubating.orkg.org/property/CSVW_Value",\n'
+                    '    "label": "http://www.w3.org/2000/01/rdf-schema#label"\n'
                     '  }\n'
                     '}')
         self.assertEqual(result, expected)
